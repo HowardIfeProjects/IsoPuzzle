97bca726f6a740dcd70bdc4628da885c91e5e82f
howardife <ifehoward@gmail.com>
2015-12-06 20:23:30 +0000

    Camera Fix

---
+++ IsoPuzzleGame_Unity/Assets/Scripts/Effects/LookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	// Update is called once per frame
+	private void Update () {
+
+        if (Camera.main == null)
+            return;
+
+        Vector3 _relativePos = Camera.main.transform.position - transform.position;
+        Quaternion _rotation = Quaternion.LookRotation(-_relativePos);
+        transform.rotation = _rotation;
+
+    }
+}
